3a08ff66ff42e200f7dbb28e27bd9a5aa34f85dd
Bruno Clementino Lamarao <Bruno.Lamarao@coolblue.nl>
2018-08-21 14:12:58 +0200

    including another feature of c#7

--- build-agent-test2/Program.cs
+++ build-agent-test2/Program.cs
@@ -6,32 +6,41 @@ namespace build_agent_test2
     {
         static void Main(string[] args)
         {
-            GlobalPosition position = new GlobalPosition(51.9233076,4.4794524);
+            GlobalPosition position = new GlobalPosition("51.9233076-N","4.4794524-W");
 
             Console.WriteLine("Hello World!");
 
             Console.WriteLine(position.PrintMe());
+
+            position.WhichHemisphere();
 
             Console.ReadLine();
         }
 
         public class GlobalPosition
         {
-            private readonly double _latitude;
-            private readonly double _longitude;
+            private readonly string _latitude;
+            private readonly string _longitude;
 
             public string Position
             {
                 get => $"{_latitude}-{_longitude}";
             }
 
-            public GlobalPosition(double latitude, double longitude)
+            public GlobalPosition(string latitude, string longitude)
             {
                 _latitude = latitude;
                 _longitude = longitude;
             }
 
             public string PrintMe() => $"Your current position is {Position}";
+
+            public void WhichHemisphere()
+            {
+                Console.WriteLine(Hemisphere());
+
+                string Hemisphere() => _latitude.Split('-')[1].Equals("N") ? "Hemisfério Norte" : "Hemisfério Sul"; 
+            }
         }
     }
 }
